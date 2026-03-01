d06c66dec0761e1873d2873ec33b63e323140a8e
有墨文档 <contact@umodoc.com>
2026-03-01 13:06:38 +0800

    github pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(() => {
+  const isGitHubPagesBuild = process.env.GITHUB_PAGES === 'true'
+
+  return {
+    base: isGitHubPagesBuild ? '/demo/' : '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
